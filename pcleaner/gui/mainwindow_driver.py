ac0946ee141916347fd4d7425ad108736940c16d
Voxel <41875513+VoxelCubes@users.noreply.github.com>
2023-08-16 23:14:57 +0200

    Add an icon to the main image tab

--- pcleaner/gui/mainwindow_driver.py
+++ pcleaner/gui/mainwindow_driver.py
@@ -80,6 +80,8 @@ class MainWindow(Qw.QMainWindow, Ui_MainWindow):
         self.file_table.table_not_empty.connect(lambda: self.stackedWidget.setCurrentIndex(1))
         # Hide the close button for the file table tab.
         self.image_tab.tabBar().setTabButton(0, Qw.QTabBar.RightSide, None)
+        # Display a theme icon on the left side of the tab.
+        self.image_tab.tabBar().setTabIcon(0, Qg.QIcon.fromTheme("view-form"))
 
         # Connect signals.
         self.connect_combobox_slots()
--- pcleaner/gui/rc_generated_files/rc_icons.py
+++ pcleaner/gui/rc_generated_files/rc_icons.py
@@ -1,6 +1,6 @@
 # Resource object code (Python 3)
 # Created by: object code
-# Created by: The Resource Compiler for Qt version 6.4.0
+# Created by: The Resource Compiler for Qt version 6.5.2
 # WARNING! All changes made in this file will be lost!
 
 from PySide6 import QtCore
@@ -348,16 +348,13 @@ qt_resource_struct = b"\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x87\x8c\xdf\x0e\xff\
+\x00\x00\x01\x87\xd4\x8d\x9c+\
 "
-
 
 def qInitResources():
     QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
 
-
 def qCleanupResources():
     QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
-
 
 qInitResources()
