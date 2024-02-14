83ac1bf3540f2e895fb6f13cc3ccfb60122299ae
Voxel <41875513+VoxelCubes@users.noreply.github.com>
2024-02-15 00:40:23 +0100

    New translations panelcleaner_source.ts (English, United States)

--- translations/PanelCleaner_en_US.ts
+++ translations/PanelCleaner_en_US.ts
@@ -1437,29 +1437,27 @@ Green: initial boxes. Red: extended boxes. Purple: merged (final) boxes. Blue: r
     </message>
     <message>
       <location filename="process_strings.py" line="40"/>
+      <source>The standard deviation (σ) and outline thickness (in pixels) of each best mask chosen, if any. Lower σ is better, from perfect (purple) to failed (red).</source>
+      <comment>Step description in the image details view, step: Masker, output: Fitment Quality</comment>
+      <translation type="unfinished">The standard deviation (σ) and outline thickness (in pixels) of each best mask chosen, if any. Lower σ is better, from perfect (purple) to failed (red).</translation>
+    </message>
+    <message>
+      <location filename="process_strings.py" line="42"/>
+      <source>Fitment Quality</source>
+      <comment>Output name in the image details view</comment>
+      <translation type="unfinished">Fitment Quality</translation>
+    </message>
+    <message>
+      <location filename="process_strings.py" line="43"/>
       <source>The input image with the final mask overlaid in color.</source>
       <comment>Step description in the image details view, step: Masker, output: Mask Overlay</comment>
       <translation type="unfinished">The input image with the final mask overlaid in color.</translation>
     </message>
     <message>
-      <location filename="process_strings.py" line="42"/>
+      <location filename="process_strings.py" line="45"/>
       <source>Mask Overlay</source>
       <comment>Output name in the image details view</comment>
       <translation type="unfinished">Mask Overlay</translation>
-    </message>
-    <message>
-      <location filename="process_strings.py" line="43"/>
-      <source>The standard deviation (σ) and outline thickness (in pixels) of each best mask chosen, if any.
-Lower σ is better, from perfect (purple) to failed (red).</source>
-      <comment>Step description in the image details view, step: Masker, output: Match Quality</comment>
-      <translation type="unfinished">The standard deviation (σ) and outline thickness (in pixels) of each best mask chosen, if any.
-Lower σ is better, from perfect (purple) to failed (red).</translation>
-    </message>
-    <message>
-      <location filename="process_strings.py" line="45"/>
-      <source>Match Quality</source>
-      <comment>Output name in the image details view</comment>
-      <translation type="unfinished">Match Quality</translation>
     </message>
     <message>
       <location filename="process_strings.py" line="46"/>
