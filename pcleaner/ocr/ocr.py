987a0fbdf8c51c4a250d8714a88c7ca7b9dd558f
Voxel <41875513+VoxelCubes@users.noreply.github.com>
2024-04-15 02:00:34 +0200

    warn of expensive initialization

--- pcleaner/ocr/ocr.py
+++ pcleaner/ocr/ocr.py
@@ -1,7 +1,5 @@
 from pathlib import Path
-from typing import Mapping
-from typing import Protocol
-from typing import TypeAlias
+from typing import Mapping, Protocol, TypeAlias
 
 from PIL import Image
 
@@ -23,10 +21,18 @@ def get_ocr_processor(profile: cfg.Profile) -> OcrProcsType:
     want_tess = profile.preprocessor.ocr_engine == cfg.OCREngine.TESSERACT
     tess_langs: set[str] = TesseractOcr.langs()
     if want_tess and not tess_langs:
+        # TODO Inform GUI users about this. A good way would probably be to have the gui 
+        #  check it at post-init, showing a warning if it fails.
+        # The gui can have a link to the readme in that case (just insert an html <a> tag, Qt will know what to
+        # do with that.
         print(
             f"Tesseract OCR is not installed or not found. "
             "Please see the instructions to install Tesseract correctly. Falling back to manga-ocr."
         )
+    # When assigning MangaOCR to multiple languages, the same instance is used due
+    # to the expensive model initialization. Tesseract meanwhile is cheap to initialize in its current state.
+    # TODO
+
     ocr_processor: OcrProcsType = {
         st.DetectedLang.JA: TesseractOcr("jpn")
         if want_tess and "jpn" in tess_langs
--- pcleaner/ocr/ocr_mangaocr.py
+++ pcleaner/ocr/ocr_mangaocr.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 
 from PIL import Image
+from loguru import logger
 
 
 class MangaOcr:
     def __init__(self, *args, **kwargs):
         from manga_ocr import MangaOcr
 
+        logger.warning("EXPENSIVE LOADING HAPPENING HERE\n\n====================\n\n")
         self._model = MangaOcr(*args, **kwargs)
 
     def __call__(self, img_or_path: Image.Image | Path | str, **kwargs) -> str:
