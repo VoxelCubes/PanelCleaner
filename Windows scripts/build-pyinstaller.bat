c2d4a2d44ea8f403b390c5b24d8a226dd6463b5b
Voxel <41875513+VoxelCubes@users.noreply.github.com>
2024-08-06 21:22:46 +0100

    Fix pyinstaller not including assets on windows

--- Windows scripts/build-pyinstaller.bat
+++ Windows scripts/build-pyinstaller.bat
@@ -5,7 +5,7 @@ call build-integration-helper-pyinstaller.bat
 .\venv\Scripts\pip install pyinstaller
 .\venv\Scripts\pyinstaller.exe pcleaner/main.py --paths 'venv/Lib/site-packages' ^
     --onedir --noconfirm --clean --workpath=build --distpath=dist_exe --windowed ^
-    --name="PanelCleaner" --icon=icons\logo.ico ^
+    --name="PanelCleaner" --icon=pcleaner/data/custom_icons/logo.ico ^
     --copy-metadata filelock ^
     --copy-metadata huggingface-hub ^
     --copy-metadata numpy ^
@@ -22,6 +22,9 @@ call build-integration-helper-pyinstaller.bat
     --hidden-import=scipy.signal ^
     --add-data "dist_exe/WindowsExplorerIntegrationRegedit.exe;pcleaner/data/" ^
     --add-data "venv/Lib/site-packages/manga_ocr/assets/example.jpg;assets/" ^
-    --collect-data pcleaner
+    --collect-datas pcleaner
 
 Copy "docs\What is _internal.txt" "dist_exe/PanelCleaner\What is _internal.txt"
+xcopy "pcleaner\data" "dist_exe\PanelCleaner\_internal\pcleaner\data" /E /I /Y
+cd "dist_exe\PanelCleaner\_internal\pcleaner\data"
+for /d /r . %%d in (__pycache__) do @rmdir /s /q "%%d"
